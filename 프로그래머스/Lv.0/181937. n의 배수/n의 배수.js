f3896791bd36042a50213457708c9451a2d12e8c
SeungMi Woo <104605709+wSeungMi@users.noreply.github.com>
2024-01-12 07:55:02 +0900

    [level Lv.0] Title: n의 배수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181937. n의 배수/n의 배수.js
@@ -0,0 +1,10 @@
+function solution(num, n) {
+    var answer = 0;
+    let multi = num % n;
+    
+    if(multi === 0) {
+        return answer = 1;
+    } else {
+        return answer = 0;
+    }
+}
