74607bb3a2a8b11edcad8dc575388073b26c9558
Seungmi Woo <104605709+wSeungMi@users.noreply.github.com>
2024-01-21 11:24:24 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,10 +1,5 @@
 function solution(a, d, included) {
     var answer = 0;
-    for(let i = 0; i < included.length; i++) {
-        if(included[i]) {
-            answer += a+(d*i);
-        }
-    }
-    
+    console.log(included.length)
     return answer;
 }
